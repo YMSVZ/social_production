c2743612af9af53b9132de097a9ebc9a353d6b19
jvanz967 <jvanz967@gmail.com>
2026-02-18 17:29:40 +0300

    Initial commit: complete frontend with mock data

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './index.css'
+import App from './App.jsx'
+import PostPage from './PostPage.jsx'
+import CreatePage from './CreatePage.jsx'
+import AuthPage from './AuthPage.jsx'
+import ChannelPage from './ChannelPage.jsx'
+import UserProfile from './UserProfile.jsx'
+import CreateChannelPage from './CreateChannelPage.jsx'
+import EditProjectPage from './EditProjectPage.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/post/:id" element={<PostPage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/channel/:name" element={<ChannelPage />} />
+        <Route path="/user/:username" element={<UserProfile />} />
+        <Route path="/create-channel" element={<CreateChannelPage />} />
+        <Route path="/edit-project/:id" element={<EditProjectPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
